cd8fa8ef3cf1905fba5a0294cc104de59fa45c05
Ishara-Mac <43241416+Ishara-MacDonald@users.noreply.github.com>
2022-02-20 19:23:24 +0100

    initial commit

---
+++ src/main/java/dev/ismacd/artportfolioproject/ArtPortfolioProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.ismacd.artportfolioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPortfolioProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtPortfolioProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ismacd/artportfolioproject/ArtPortfolioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ismacd.artportfolioproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtPortfolioProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
